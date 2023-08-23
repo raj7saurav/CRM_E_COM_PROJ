e6f6c1a23ee9bf189eb80072a8be90b31434c30b
Saurav Raj <raj7saurav@gmail.com>
2023-08-23 14:30:44 +0530

    Connecting to DB

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,3 +1,3 @@
 module.exports={
-    DB_URL:"mongodb://localhost/crm_db"
+    DB_URL:"mongodb://locolhost/crm_db"
 }
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 //This should be starting point of the server
 
+const dbConfig = require("./configs/db.config");
+const express = require("express");
 const serverConfig = require("./configs/server.config");
-const dbConfig = require("./configs/server.config");
-const express=require('express');
-
 const app = express();
+
+const mongoose = require("mongoose"); 
+
+
+//I need to connect to the database
+
+mongoose.connect(dbConfig.DB_URL);
+
+const db = mongoose.connection;
+
+db.on('error' , () =>{
+    console.log("Error wile connecting to db");
+});
+
+db.once("open" , () => {
+    console.log("Connected with database");
+});
+
 
 app.listen(serverConfig.PORT,() =>{
     console.log("Server started on the port no:" , serverConfig.PORT);
-    
+
 })
